c39cbfb18abb757e415717ffe0dd0ed402ae6879
Rodney Madondo <rodney@finfind.co.za>
2024-11-15 11:20:22 +0200

    completed mobile css of home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/*.{html,js}",
-    "./public/index.html"
+    "./src/**/*.{html,js}",    
+    "./public/**/*.html",
   ],
   theme: {
     extend: {
-      // backgroundImage:{
-      //   'home-pattern':"url('./assets/home/background-home-mobile.jpg')"
-      // }
+      colors:{
+        primary:"#0B0D17",
+        secondary:"#D0D6F9",
+      },
+      fontFamily:{
+        sans:["'Barlow Condensed'", 'sans-serif'],
+        serif:["'Bellefair'", 'serif'],
+      }
     },
   },
   plugins: [],
